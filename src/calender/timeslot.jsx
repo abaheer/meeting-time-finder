c583d1e1cef93dc567f08f661d5807b521910389
bahm941 <baheer1234@live.com>
2024-03-21 13:17:42 +1300

    Store updated times in map

--- src/calender/timeslot.jsx
+++ src/calender/timeslot.jsx
@@ -23,12 +23,36 @@ export const TimeSlots = (props) => {
     end: new Date(2014, 9, 6, 17),
   });
 
-  const [freeHours, setFreeHours] = useState([]);
+  const [freeHours, setFreeHours] = useState(new Map());
 
   const setAvailable = (date) => {
     setFreeHours((prev) => {
-      console.log([...prev, date]);
-      return [...prev, date]; // Return a new array with the updated date
+      const newMap = prev; // Create a shallow copy of the previous map
+      const day = date.toLocaleDateString("en-US");
+      const hour = date.getHours();
+
+      if (!newMap.has(day)) {
+        newMap.set(day, [date]);
+      } else {
+        // If the key exists, check if the hour is already in the array
+        const hoursArray = newMap.get(day);
+        if (!hoursArray.includes(hour)) {
+          // If the hour doesn't exist, add it to the array
+          hoursArray.push(hour);
+          newMap.set(day, hoursArray); // Update the value for the key
+        } else {
+          const hoursArray = newMap.get(day);
+          newMap.set(
+            day,
+            hoursArray.filter(function (e) {
+              return e !== hour;
+            })
+          );
+        }
+      }
+
+      console.log(newMap);
+      return newMap; // Return the new Map instance
     });
   };
 
