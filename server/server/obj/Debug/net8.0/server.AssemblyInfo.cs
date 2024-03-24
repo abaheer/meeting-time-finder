257fd0f034fd5545fcae3b4df4d144e9633eec1f
bahm941 <baheer1234@live.com>
2024-03-24 18:07:15 +1300

    Create controller classes

--- server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012fe13da10d13f836aea1b8c491d145304a01a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67573e6413faf7b8135aae20eb26a254418a4ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
